c698dc3e7ba4bba8f4217b2bdba67e3251e3b613
ST-Sebastian <djkrjcx@gmail.com>
2021-01-16 17:23:15 +0000

    MongoDB implementation

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const config = require('../slappey.json');
 const client = new Client();
+const mongo = require('./mongo');
 
 (async () => {
   client.commands = new Map();
   client.events = new Map();
   client.prefix = config.prefix;
+  await mongo().then(mongoose => {
+    try {
+      console.log('Connected to Database!');
+    } finally {
+      mongoose.connection.close();
+    }
+  })
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.DJS_TOKEN);
