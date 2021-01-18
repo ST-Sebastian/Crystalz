3a358303c4307d1ac786b45cebbd6a0b234eec78
ST-Sebastian <djkrjcx@gmail.com>
2021-01-18 09:39:22 +0000

    Welcome command push

--- src/commands/moderation/SetWelcomeCommand.js
+++ src/commands/moderation/SetWelcomeCommand.js
@@ -1,6 +1,7 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const mongo = require('../../../mongo');
 const Discord = require('discord.js');
+const welcomeSchema = require('../../../schemas/welcome-schema');
 
 module.exports = class SetWelcomeCommand extends BaseCommand {
   constructor() {
@@ -8,8 +9,23 @@ module.exports = class SetWelcomeCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
+    const { member, channel, content, guild } = message
+
     if (!member.hasPermission('ADMINISTRATOR')) {
-      message.channel.send('Testing for now');
+      channel.send('You do not have permission to run that command!');
+      return;
     }
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await new welcomeSchema({
+          _id: guild.id,
+          channelId: channel.id,
+          text: content
+        }).save()
+      } finally {
+        mongoose.conenction.close();
+      }
+    });
   }
 }
